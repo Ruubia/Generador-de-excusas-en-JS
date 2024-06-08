5920ded5b5198786ae9948ab27e291a0f56ebebb
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-06-08 13:46:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRan = Math.floor(Math.random() * who.length);
+  let actionRan = Math.floor(Math.random() * action.length);
+  let whatRan = Math.floor(Math.random() * what.length);
+  let whenRan = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[whoRan] +
+    " " +
+    action[actionRan] +
+    " " +
+    what[whatRan] +
+    " " +
+    when[whenRan];
+
+  document.getElementById("excuse").innerText = excuse;
 };
